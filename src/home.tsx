364e263847bf8fdf2e6fa3a8bc05a1494d0d203f
mikeb2113 <mikebermudez2113@gmail.com>
2026-07-24 14:45:39 -0400

    fixing CORS

--- src/home.tsx
+++ src/home.tsx
@@ -886,7 +886,7 @@ function ContactSection() {
     if (!inputVal.trim()) return;
 
     try {
-      const response = await fetch("https://portfolio-emailer-91q1.onrender.com", {
+      const response = await fetch("https://portfolio-emailer-91q1.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
